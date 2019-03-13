d0494da9d37e165f141ee85801970f21499f8d26
David Dai <d4dai@ucsd.edu>
2019-03-13 15:32:17 -0700

    Fixed Blinking

--- LightSwitch/LightSwitch.ino
+++ LightSwitch/LightSwitch.ino
@@ -1,5 +1,5 @@
 char inByte;
-
+bool off = true;
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
@@ -11,15 +11,26 @@ void loop() {
     inByte = Serial.read(); // read the incoming data
     Serial.println(inByte); // send the data back in a new line so that it is not all one long line
     if(inByte == '0'){
+      off = true;
       digitalWrite(LED_BUILTIN, LOW);
+      delay(1000);
     }
     else if(inByte == '1'){
+      off = false;
       digitalWrite(LED_BUILTIN, HIGH);
+      delay(1000);
     }
   }
   else{
-    Serial.println("No input");
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    if(off == false){
+      digitalWrite(LED_BUILTIN, LOW);  
+      delay(1000);
+      // turn the LED off by making the voltage LOW
+    }
+    else if(off == true){
+       digitalWrite(LED_BUILTIN, HIGH); 
+       delay(1000); 
+    }
     delay(100);
   }
 }
